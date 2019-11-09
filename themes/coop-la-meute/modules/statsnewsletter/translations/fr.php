1b1ac4f4baae9e3dcc9533a0a32732f299399872
nmaurent <nbowlinger@yahoo.com>
2019-11-09 03:20:49 +0100

    Just after update script to 1.6.1.24

--- themes/coop-la-meute/modules/statsnewsletter/translations/fr.php
+++ themes/coop-la-meute/modules/statsnewsletter/translations/fr.php
@@ -2,13 +2,13 @@
 
 global $_MODULE;
 $_MODULE = array();
-$_MODULE['<{statsnewsletter}coop-la-meute-1.6.1.17>statsnewsletter_ffb7e666a70151215b4c55c6268d7d72'] = 'Lettre d\'informations';
-$_MODULE['<{statsnewsletter}coop-la-meute-1.6.1.17>statsnewsletter_5106250927c6e24c99601968284066de'] = 'Ajoute un onglet avec un graphique montrant l\'évolution des enregistrement à la newsletter dans votre tableau des statistiques.';
-$_MODULE['<{statsnewsletter}coop-la-meute-1.6.1.17>statsnewsletter_61a898af87607e3f4d41c3613d8761c7'] = 'Inscriptions depuis le compte client :';
-$_MODULE['<{statsnewsletter}coop-la-meute-1.6.1.17>statsnewsletter_7fe462207f98012d9ff00cf0e6633c94'] = 'Inscriptions depuis le bloc sur le front-office :';
-$_MODULE['<{statsnewsletter}coop-la-meute-1.6.1.17>statsnewsletter_64342cd480b27dfeefb08bace6e82fdc'] = 'Les deux :';
-$_MODULE['<{statsnewsletter}coop-la-meute-1.6.1.17>statsnewsletter_998e4c5c80f27dec552e99dfed34889a'] = 'Export CSV';
-$_MODULE['<{statsnewsletter}coop-la-meute-1.6.1.17>statsnewsletter_cf74c2815ab62be1efa55a4a5d3f46a4'] = 'Statistiques de la newsletter';
-$_MODULE['<{statsnewsletter}coop-la-meute-1.6.1.17>statsnewsletter_4b6f7d34a58ba399f077685951d06738'] = 'Clients';
-$_MODULE['<{statsnewsletter}coop-la-meute-1.6.1.17>statsnewsletter_ae5d01b6efa819cc7a7c05a8c57fcc2c'] = 'Visiteurs';
-$_MODULE['<{statsnewsletter}coop-la-meute-1.6.1.17>statsnewsletter_130c5b3473c57faa76e2a1c54e26f88e'] = 'Les deux';
+$_MODULE['<{statsnewsletter}coop-la-meute>statsnewsletter_ffb7e666a70151215b4c55c6268d7d72'] = 'Lettre d\'informations';
+$_MODULE['<{statsnewsletter}coop-la-meute>statsnewsletter_5106250927c6e24c99601968284066de'] = 'Ajoute un onglet avec un graphique montrant l\'évolution des enregistrement à la newsletter dans votre tableau des statistiques.';
+$_MODULE['<{statsnewsletter}coop-la-meute>statsnewsletter_61a898af87607e3f4d41c3613d8761c7'] = 'Inscriptions depuis le compte client :';
+$_MODULE['<{statsnewsletter}coop-la-meute>statsnewsletter_7fe462207f98012d9ff00cf0e6633c94'] = 'Inscriptions depuis le bloc sur le front-office :';
+$_MODULE['<{statsnewsletter}coop-la-meute>statsnewsletter_64342cd480b27dfeefb08bace6e82fdc'] = 'Les deux :';
+$_MODULE['<{statsnewsletter}coop-la-meute>statsnewsletter_998e4c5c80f27dec552e99dfed34889a'] = 'Export CSV';
+$_MODULE['<{statsnewsletter}coop-la-meute>statsnewsletter_cf74c2815ab62be1efa55a4a5d3f46a4'] = 'Statistiques de la newsletter';
+$_MODULE['<{statsnewsletter}coop-la-meute>statsnewsletter_4b6f7d34a58ba399f077685951d06738'] = 'Clients';
+$_MODULE['<{statsnewsletter}coop-la-meute>statsnewsletter_ae5d01b6efa819cc7a7c05a8c57fcc2c'] = 'Visiteurs';
+$_MODULE['<{statsnewsletter}coop-la-meute>statsnewsletter_130c5b3473c57faa76e2a1c54e26f88e'] = 'Les deux';
