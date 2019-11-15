c5758e904308ab5071c12e939d3eecf3934d702f
nmaurent <nbowlinger@yahoo.com>
2019-11-15 23:36:58 +0100

    Clear reference and ean13 bar code when duplicating product

--- override/controllers/admin/AdminProductsController.php
+++ override/controllers/admin/AdminProductsController.php
@@ -32,9 +32,62 @@ class AdminProductsController extends AdminProductsControllerCore
 			),
 			array_slice($this->fields_list,$image_offset-1)
 		);
-	}
+    }
+    
+    public function processDuplicate()
+    {
+        if (Validate::isLoadedObject($product = new Product((int)Tools::getValue('id_product')))) {
+            $id_product_old = $product->id;
+            if (empty($product->price) && Shop::getContext() == Shop::CONTEXT_GROUP) {
+                $shops = ShopGroup::getShopsFromGroup(Shop::getContextShopGroupID());
+                foreach ($shops as $shop) {
+                    if ($product->isAssociatedToShop($shop['id_shop'])) {
+                        $product_price = new Product($id_product_old, false, null, $shop['id_shop']);
+                        $product->price = $product_price->price;
+                    }
+                }
+            }
+            unset($product->id);
+            unset($product->id_product);
+            // Nicolas MAURENT - 15.11.2019 - Clear reference and ean13 bar code when duplicating product
+            unset($product->reference);
+            unset($product->ean13);
+            // Nicolas MAURENT - 15.11.2019 - End
+            $product->indexed = 0;
+            $product->active = 0;
+            if ($product->add()
+            && Category::duplicateProductCategories($id_product_old, $product->id)
+            && Product::duplicateSuppliers($id_product_old, $product->id)
+            && ($combination_images = Product::duplicateAttributes($id_product_old, $product->id)) !== false
+            && GroupReduction::duplicateReduction($id_product_old, $product->id)
+            && Product::duplicateAccessories($id_product_old, $product->id)
+            && Product::duplicateFeatures($id_product_old, $product->id)
+            && Pack::duplicate($id_product_old, $product->id)
+            && Product::duplicateCustomizationFields($id_product_old, $product->id)
+            && Product::duplicateTags($id_product_old, $product->id)
+            && Product::duplicateDownload($id_product_old, $product->id)) {
+                if ($product->hasAttributes()) {
+                    Product::updateDefaultAttribute($product->id);
+                } else {
+                    Product::duplicateSpecificPrices($id_product_old, $product->id);
+                }
+
+                if (!Tools::getValue('noimage') && !Image::duplicateProductImages($id_product_old, $product->id, $combination_images)) {
+                    $this->errors[] = Tools::displayError('An error occurred while copying images.');
+                } else {
+                    Hook::exec('actionProductAdd', array('id_product' => (int)$product->id, 'product' => $product));
+                    if (in_array($product->visibility, array('both', 'search')) && Configuration::get('PS_SEARCH_INDEXATION')) {
+                        Search::indexation(false, $product->id);
+                    }
+                    $this->redirect_after = self::$currentIndex.(Tools::getIsset('id_category') ? '&id_category='.(int)Tools::getValue('id_category') : '').'&conf=19&token='.$this->token;
+                }
+            } else {
+                $this->errors[] = Tools::displayError('An error occurred while creating an object.');
+            }
+        }
+    }
 	
-	public function processUpdate()
+    public function processUpdate()
     {
         $existing_product = $this->object;
 
