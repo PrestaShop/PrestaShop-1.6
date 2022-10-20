16fe37376508feaec61ef58f372a90d3c07980a4
Nicolas MAURENT <nbowlinger@gmail.com>
2022-10-20 02:32:15 +0200

    Enable parametrization of CSV file seperator in SQL export with PS_CSV_SEPARATOR_MANAGER_SQL

--- override/controllers/admin/AdminRequestSqlController.php
+++ override/controllers/admin/AdminRequestSqlController.php
@@ -13,6 +13,69 @@
 class AdminRequestSqlController extends AdminRequestSqlControllerCore
 {
 
+  // Nicolas MAURENT - 20.10.2022 - Introduce csv separator configuration - Start
+  // PS_CSV_SEPARATOR_MANAGER_SQL to be added into ps_configuration table
+  /**
+   * @var array : List of separator for a csv file
+   */
+   public static $csv_separator = array(
+      array('value' => 1, 'name' => ';'),
+      array('value' => 2, 'name' => ',')
+  );
+
+  public function __construct()
+  {
+      $this->bootstrap = true;
+      $this->table = 'request_sql';
+      $this->className = 'RequestSql';
+      $this->lang = false;
+      $this->export = true;
+
+      $this->context = Context::getContext();
+
+      parent::__construct();
+
+      $this->fields_list = array(
+          'id_request_sql' => array('title' => $this->l('ID'), 'class' => 'fixed-width-xs'),
+          'name' => array('title' => $this->l('SQL query Name')),
+          'sql' => array('title' => $this->l('SQL query'))
+      );
+
+      $this->fields_options = array(
+          'general' => array(
+              'title' =>    $this->l('Settings'),
+              'fields' =>    array(
+                  'PS_ENCODING_FILE_MANAGER_SQL' => array(
+                      'title' => $this->l('Select your default file encoding'),
+                      'cast' => 'intval',
+                      'type' => 'select',
+                      'identifier' => 'value',
+                      'list' => self::$encoding_file,
+                      'visibility' => Shop::CONTEXT_ALL
+                  ),
+                  'PS_CSV_SEPARATOR_MANAGER_SQL' => array(
+                      'title' => $this->l('Select your default CSV seperator'),
+                      'cast' => 'intval',
+                      'type' => 'select',
+                      'identifier' => 'value',
+                      'list' => static::$csv_separator,
+                      'visibility' => Shop::CONTEXT_ALL
+                  )
+              ),
+              'submit' => array('title' => $this->l('Save'))
+          )
+      );
+
+      $this->bulk_actions = array(
+          'delete' => array(
+              'text' => $this->l('Delete selected'),
+              'confirm' => $this->l('Delete selected items?'),
+              'icon' => 'icon-trash'
+          )
+      );
+  }
+  // Nicolas MAURENT - 20.10.2022 - Introduce csv separator configuration - End
+
     /**
      * Genrating a export file
      */
@@ -29,19 +92,34 @@ class AdminRequestSqlController extends AdminRequestSqlControllerCore
 
             if ($sql) {
                 $results = Db::getInstance()->executeS($sql[0]['sql']);
+                // Nicolas MAURENT - 20.10.2022 - Use csv seperator accoring to configuration - Start
+                $separator_name = static::$csv_separator[0]['name'];
+                if (Configuration::get('PS_CSV_SEPARATOR_MANAGER_SQL')) {
+                    $PS_CSV_SEPARATOR_MANAGER_SQL = Configuration::get('PS_CSV_SEPARATOR_MANAGER_SQL');
+                    for($x = 0; $x < count(static::$csv_separator); $x++) {
+                      if (static::$csv_separator[$x]['value'] == $PS_CSV_SEPARATOR_MANAGER_SQL) {
+                        $separator_name = static::$csv_separator[$x]['name'];
+                      }
+                    }
+                }
+
                 foreach (array_keys($results[0]) as $key) {
                     $tab_key[] = $key;
-                    fputs($csv, $key.';');
+                    //fputs($csv, $key.';');
+                    fputs($csv, $key.$separator_name);
                 }
                 foreach ($results as $result) {
                     fputs($csv, "\n");
                     foreach ($tab_key as $name) {
-                        fputs($csv, strip_tags($result[$name]).';');
+                        //fputs($csv, strip_tags($result[$name]).';');
+                        fputs($csv, strip_tags($result[$name]).$separator_name);
                     }
                 }
+                // Nicolas MAURENT - 20.10.2022 - Use csv seperator accoring to configuration - End
+
                 if (file_exists($export_dir.$file)) {
                     $filesize = filesize($export_dir.$file);
-                    // Nicolas MAURENT - 19.10.2022 - Pickup right charset name to set Content-Type
+                    // Nicolas MAURENT - 19.10.2022 - Pickup right charset name to set Content-Type - Start
                     $charset = self::$encoding_file[0]['name'];
                     if (Configuration::get('PS_ENCODING_FILE_MANAGER_SQL')) {
                         //$charset = Configuration::get('PS_ENCODING_FILE_MANAGER_SQL');
@@ -66,6 +144,8 @@ class AdminRequestSqlController extends AdminRequestSqlControllerCore
                     } else {
                         header('Content-Length: '.$filesize);
                     }
+                    // Nicolas MAURENT - 19.10.2022 - Pickup right charset name to set Content-Type - End
+
                     readfile($export_dir.$file);
                     die();
                 }
